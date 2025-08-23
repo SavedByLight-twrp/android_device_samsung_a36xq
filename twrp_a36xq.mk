b4de35f145b48ad0ee04a14adbe846463c4f12d4
Jamie Falconer <mrfluffyoven@gmail.com>
2025-08-23 12:19:22 +0100

    cleanup inherits

--- twrp_a36xq.mk
+++ twrp_a36xq.mk
@@ -1,28 +1,27 @@
-#
-# Copyright (C) 2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Release name
+PRODUCT_RELEASE_NAME := a36xq
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from a36xq device
-$(call inherit-product, device/samsung/a36xq/device.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-PRODUCT_DEVICE := a36xq
-PRODUCT_NAME := twrp_a36xq
+# Inherit from device
+$(call inherit-product, device/samsung/$(PRODUCT_RELEASE_NAME)/device.mk)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/$(PRODUCT_RELEASE_NAME)/recovery/root,recovery/root)
+
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A366E
 PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a36xqnsxx-user 15 AP3A.240905.015.A2 A366EXXU1AYB9 release-keys"
-
-BUILD_FINGERPRINT := samsung/a36xqnsxx/qssi_64:15/AP3A.240905.015.A2/A366EXXU1AYB9:user/release-keys
