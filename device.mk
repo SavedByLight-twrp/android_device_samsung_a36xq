cc77c0aeca165f9d1befd84f72f43decb0f1f618
SavedByLight <mrfluffyoven@gmail.com>
2025-08-23 12:08:55 +0100

    test: seperate A/B stuff

--- device.mk
+++ device.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := device/samsung/a36x
 A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 AB_OTA_UPDATER := true
+
 AB_OTA_PARTITIONS += \
     system_dlkm \
     vendor \
